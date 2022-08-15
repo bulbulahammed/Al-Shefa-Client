05c26cef5d9b14c2bd4278e1660ff8f651b92c3a
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-15 19:11:27 +0600

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFECed",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
   plugins: [require("daisyui")],
 }
