23c1319f6ab12380daef1f42b5d9ce0fc87db392
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-16 16:39:39 +0600

    Complete navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import './App.css';
 import About from "./pages/About/About";
 import Home from './pages/Home/Home';
+import Login from "./pages/Login/Login";
 import Navbar from './pages/Shared/Navbar';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   );
