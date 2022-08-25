c8ab2fa08bea3b56dea7e46612dc9563528b8501
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-25 12:42:27 +0600

    Connect mongoDb and delete sample data json

--- src/pages/Appointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment.js
@@ -9,7 +9,7 @@ const AvailableAppointment = ({date}) => {
 
 
     useEffect(()=>{
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
         .then(res=> res.json())
         .then(data=>setServices(data))
     },[])
