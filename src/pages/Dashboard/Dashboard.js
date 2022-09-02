9b6b4216fa291621883461bbb960a16232f93c5c
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-02 16:19:06 +0600

    Display User specific Appointments using Data Table

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,16 +3,14 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
 <div className="drawer drawer-mobile">
-  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content flex flex-col items-center justify-center">
+  <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
     {/* <!-- Page content here --> */}
-    <h2 className='text-5xl text-center text-purple-700'>Dashboard</h2>
+    <h2 className='text-2xl text-center text-purple-700 font-bold'>Welcome to your Dashboard</h2>
     <Outlet></Outlet>
-    <label for="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-  
   </div> 
   <div className="drawer-side">
-    <label for="my-drawer-2" className="drawer-overlay"></label> 
+    <label for="dashboard-sidebar" className="drawer-overlay"></label> 
     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
       {/* <!-- Sidebar content here --> */}
       <li><Link to="/dashboard">Dashboard</Link></li>
