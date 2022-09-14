e10b1949d795898d954fa692dbd2d6df51dadbac
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-14 12:17:31 +0600

    Show user in Dashboard

--- src/pages/Dashboard/Users.js
+++ src/pages/Dashboard/Users.js
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import UserRow from './UserRow';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
-
-    // const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
-    //     method: 'GET',
-    //     headers:{
-    //         authorization: `Bearer ${localStorage.getItem('accessToken')}`
-    //     }
-    // }).then(res => res.json()));
-    // if (isLoading) {
-    //     return <Loading></Loading>
-    // }
 
     useEffect(() => {
         fetch('http://localhost:5000/user',{
@@ -28,6 +19,27 @@ const Users = () => {
     return (
         <div>
             <h2 className="text-2xl">All Users: {users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {
+                           users.map(user=><UserRow
+                           key={user._id}
+                           user={user}
+                        //    refetch={refetch}
+                           ></UserRow>)
+                       }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
