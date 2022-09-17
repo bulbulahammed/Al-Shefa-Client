1b18a47def7e89e43a5b06a7cb2e30cd6cc0538a
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-17 16:00:28 +0600

    Prevent accessing Admin route via URL

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import './App.css';
 import About from "./pages/About/About";
 import Appointment from "./pages/Appointment/Appointment";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import History from "./pages/Dashboard/History";
 import MyAppointment from './pages/Dashboard/MyAppointment';
 import MyReview from './pages/Dashboard/MyReview';
 import Users from "./pages/Dashboard/Users";
-import History from "./pages/Dashboard/History";
 import Home from './pages/Home/Home';
 import Login from "./pages/Login/Login";
+import RequireAdmin from './pages/Login/RequireAdmin';
 import RequireAuth from "./pages/Login/RequireAuth";
 import SignUp from "./pages/Login/SignUp";
 import Navbar from './pages/Shared/Navbar';
@@ -44,7 +45,9 @@ function App() {
             {/* Nester Route */}
             <Route index element={<MyAppointment></MyAppointment>}></Route>
             <Route path="review" element={<MyReview></MyReview>}></Route>
-            <Route path="users" element={<Users></Users>}></Route>
+            <Route path="users" element={<RequireAdmin>
+              <Users></Users>
+            </RequireAdmin>}></Route>
             <Route path="history" element={<History></History>}></Route>
         </Route>
       </Routes>
