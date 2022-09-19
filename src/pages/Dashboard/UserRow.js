48d986572ac307497359e10fe0922e605409e240
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-19 23:55:47 +0600

    Toast Message after set Admin

--- src/pages/Dashboard/UserRow.js
+++ src/pages/Dashboard/UserRow.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 
-const UserRow = ({ user, refetch }) => {
-    const { email, role } = user;
-    const makeAdmin = () => {
+const UserRow = ({user,refetch}) => {
+    const {email,role} = user;
+    const makeAdmin = () =>{
         fetch(`https://al-shefa-server.herokuapp.com/user/admin/${email}`, {
             method: 'PUT',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-            .then(res => {
-                if(res.status === 403){
-                    toast.error('Failed to Make an admin');
-                }
-                return res.json()})
-            .then(data => {
-                if (data.modifiedCount > 0) {
-                    refetch();
-                    toast.success(`Successfully made an admin`);
-                }
-
-            })
+        .then(res => res.json())
+        .then(data=>{
+            console.log(data);
+            refetch();
+            toast.success('Successfully Set Admin');
+        })
     }
     return (
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
-            <td><button className="btn btn-xs">Remove User</button></td>
+            <td>{role !== "admin" && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
+            <td><button className="btn btn-xs">Remove</button></td>
         </tr>
     );
 };
