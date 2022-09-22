2f2c5381ee48db55863dc15f3b1ffa75de15d9d3
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-22 16:30:31 +0600

    Upload image to imgbb server and get image url

--- src/pages/Dashboard/AddDoctor.js
+++ src/pages/Dashboard/AddDoctor.js
@@ -2,116 +2,152 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
+
+
 const AddDoctor = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
-
+    const imgStorageKey = "217c7fe015a3f8046407326d976c0197";
     const onSubmitForm = async data =>{
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image',image)
+        const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`;
         console.log("data",data);
+        fetch(url,{
+            method: 'POST',
+            body: formData,
+        })
+        .then(res =>res.json())
+        .then(result =>{
+            if(result.success){
+                const img = result.data.url;
+                const doctor = {
+                    name: data.name,
+                    email: data.email,
+                    specialty:data.specialty,
+                    img: img,
+                }
+                // Send it to Database
+                
+            }
+            console.log('imgbb', result)
+        })
     }
-    const { data: services, isLoading, refetch } = useQuery(['users'], () => fetch('http://localhost:5000/service/'
-    , {
-        method: 'GET',
-        headers: {
-            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    
+    const { data: services, isLoading, refetch } = useQuery(['users'], () => fetch('http://localhost:5000/service/',
+        {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
         }
-    }
     ).then(res => res.json()));
     if (isLoading) {
         return <Loading></Loading>
     }
     return (
-        <div className=''>
+        <div>
             <h2>Add Doctor</h2>
-            <h2>Total services: {services.length}</h2>
             <form onSubmit={handleSubmit(onSubmitForm)}>
-                            {/*----------- Label For Password ------------*/}
-                            <label className='label'>
-                                <span className='label-text text-sm'>Name</span>
-                            </label>
-                            <input
-                                type="text"
-                                className="input input-bordered w-full max-w-xs"
-                                name="name"
-                                placeholder=""
-                                {...register('name', {
-                                    required: {
-                                        value: true,
-                                        message: "You must enter Doctor's Name",
-                                    }
-                                })} />
-                                {/* Error Message */}
-                            <label className="label">
-                                {errors.name?.type === "required" && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                            </label>
-                            <label className='label'>
-                                <span className='label-text text-sm'>Email</span>
-                            </label>
-                            <input
-                                type="email"
-                                className="input input-bordered w-full max-w-xs"
-                                name="email"
-                                placeholder=""
-                                {...register('email', {
-                                    required: {
-                                        value: true,
-                                        message: "You must enter Doctor's email address",
-                                    },
-                                    minLength: {
-                                        value: 8,
-                                        message: 'This is not long enough to be an email',
-                                    },
-                                    maxLength: {
-                                        value: 120,
-                                        message: 'This is too long',
-                                    },
-                                })} />
-                                {/* Error Message */}
-                                <label className="label">
-                                    {errors.email?.type === "required" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                {/*----------- Label For Password ------------*/}
+                <label className='label'>
+                    <span className='label-text text-sm'>Name</span>
+                </label>
+                <input
+                    type="text"
+                    className="input input-bordered w-full max-w-xs"
+                    name="name"
+                    placeholder="Type Your Name"
+                    {...register('name', {
+                        required: {
+                        value: true,
+                        message: "You must enter Doctor's Name",
+                        }
+                    })} 
+                />
+                {/* Error Message */}
+                <label className="label">
+                    {errors.name?.type === "required" && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                </label>
 
-                                    {errors.email?.type === "minLength" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                {/* Label for Email Input */}
+                <label className='label'>
+                    <span className='label-text text-sm'>Email</span>
+                </label>
+                {/*Doctor's Email Input */}
+                <input
+                    type="email"
+                    className="input input-bordered w-full max-w-xs"
+                    name="email"
+                    placeholder="Type Your Email"
+                    {...register('email', {
+                        required: {
+                            value: true,
+                            message: "You must enter Doctor's email address",
+                        },
+                        minLength: {
+                            value: 8,
+                            message: 'This is not long enough to be an email',
+                        },
+                        maxLength: {
+                            value: 120,
+                            message: 'This is too long',
+                        },
+                    })} 
+                />
 
-                                    {errors.email?.type === "maxLength" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-
-                                    {errors.email?.type === "pattern" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                                </label>
+                {/*Email input  Error Message */}
+                <label className="label">
+                    {errors.email?.type === "required" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                    {errors.email?.type === "minLength" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                    {errors.email?.type === "maxLength" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                    {errors.email?.type === "pattern" && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                </label>
                                 
-                                {/* Doctor's Specialty */}
-                            <label className='label'>
-                                <span className='label-text text-sm'>Specialty</span>
-                            </label>
-                            <select {...register('specialty')} className="select select-bordered w-full max-w-xs">
-                                {
-                                    services.map(service => <option
-                                    key={service._id}
-                                    value="service.name"
-                                    >{service.name}</option>)
-                                }
-                            </select>
-                            {/* Doctors Photo */}
-                            <label className='label'>
-                                <span className='label-text text-sm'>Photo</span>
-                            </label>
-                            <input
-                                type="file"
-                                className="input input-bordered w-full max-w-xs"
-                                name="name"
-                                placeholder=""
-                                {...register('name', {
-                                    required: {
-                                        value: true,
-                                        message: "You must enter Doctor's Name",
-                                    }
-                                })} />
-                                {/* Error Message */}
-                            <label className="label">
-                                {errors.name?.type === "required" && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                            </label>
-                            <button
-                                type="submit"
-                                className="w-full max-w-xs text-center py-3 rounded btn-accent text-white hover:bg-green-dark focus:outline-none my-1"
-                            >Add</button>
-                        </form>
+                {/*Select Doctor's Specialty */}
+                <label className='label'>
+                    <span className='label-text text-sm'>Specialty</span>
+                </label>
+                <select 
+                    {...register('specialty')} 
+                    className="select select-bordered w-full max-w-xs">
+                        {
+                            services.map(service => 
+                            <option
+                            key={service._id}
+                            value={service.name}
+                            >
+                             {service.name}</option>)
+                        }
+                </select>
+
+                {/*Label for  Doctors Photo */}
+                <label className='label'>
+                    <span className='label-text text-sm'>Photo</span>
+                </label>
+                {/* Doctor's photo Input */}
+                <input
+                    type="file"
+                    className="input input-bordered w-full max-w-xs"
+                    name="image"
+                    placeholder=""
+                    {...register('image', {
+                        required: 
+                            {
+                                value: true,
+                                message: "Image is Required",
+                            }
+                    })}
+                />
+                {/* Error Message for Doctor's phot input */}
+                <label className="label">
+                    {errors.name?.type === "required" && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                </label>
+                <button
+                    type="submit"
+                    className="w-full max-w-xs text-center py-3 rounded btn-accent text-white hover:bg-green-dark focus:outline-none my-1"
+                >Add</button>
+            </form>
         </div>
     );
 };
