83b0b9c910a90a44e7ff915c4c4aba056c99ef7c
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-25 18:33:49 +0600

    Mange Doctors and delete doctor with authorization

--- src/pages/Dashboard/ManageDoctors.js
+++ src/pages/Dashboard/ManageDoctors.js
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-    const {data: doctors,isLoading} = useQuery(['doctors'],()=> fetch('http://localhost:5000/doctor',{
+    const {data: doctors,isLoading,refetch} = useQuery(['doctors'],()=> fetch('http://localhost:5000/doctor',{
         headers:{
             authorization: `Bearer ${localStorage.getItem('accessToken')}`,
         },
@@ -14,6 +15,31 @@ const ManageDoctors = () => {
     return (
         <div>
             <h2>Manage Doctor: {doctors.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                   {/* Table Head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {/* Table Row */}
+                        {
+                            doctors.map((doctor,index)=><DoctorRow
+                            key={doctor._key}
+                            doctor={doctor}
+                            index={index}
+                            refetch={refetch}
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+                </div>
         </div>
     );
 };
--- src/pages/Dashboard/UserRow.js
+++ src/pages/Dashboard/UserRow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 
-const UserRow = ({user,refetch}) => {
+const UserRow = ({user,refetch,index}) => {
     const {email,role} = user;
     const makeAdmin = () =>{
         fetch(`https://al-shefa-server.herokuapp.com/user/admin/${email}`, {
@@ -19,7 +19,7 @@ const UserRow = ({user,refetch}) => {
     }
     return (
         <tr>
-            <th>1</th>
+            <th>{index + 1}</th>
             <td>{email}</td>
             <td>{role !== "admin" && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
             <td><button className="btn btn-xs">Remove</button></td>
