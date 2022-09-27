f68e48b7889eebf0a649f50ba609056b1a381bed
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-09-27 23:56:45 +0600

    Payment Route Added

--- src/pages/Appointment/BookingModal.js
+++ src/pages/Appointment/BookingModal.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import auth from './../../firebase.init';
 
 const BookingModal = ({treatment,date,setTreatment,refetch}) => {
-    const {_id,name, slots} = treatment;
+    const {_id,name, slots,price} = treatment;
     const [user] = useAuthState(auth);
     const formattedDate = format(date,"PP")
 
@@ -20,6 +20,7 @@ const BookingModal = ({treatment,date,setTreatment,refetch}) => {
             treatment: name,
             date:formattedDate,
             slot,
+            price,
             patient: user.email,
             patientName: user.displayName,
             phone: event.target.phone.value,
--- src/pages/Dashboard/DoctorRow.js
+++ src/pages/Dashboard/DoctorRow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DoctorRow = ({doctor,index,refetch,setDeletingDoctor}) => {
-    const {name,specialty,img,email} = doctor;
+    const {name,specialty,img} = doctor;
     return (
         <>
             <tr>
