16c48a3f23c44672cd39a38831df803233786df0
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-10-05 23:54:06 +0600

    Handle payment processing error and success

--- src/pages/Dashboard/CheckoutForm.js
+++ src/pages/Dashboard/CheckoutForm.js
@@ -66,8 +66,8 @@ export default CheckoutForm;
  * 4.Create Elements Wrapper using Publishable key
  * 5.Create Checkout Form using Card Element
  * 6.Get Card Element Info(Credit Card Info)
- * 7.Get Credit Card Info/Error
- * 8.
- * 
+ * 7.Get Credit Card Info/Error ----> Display Card Error (If Any)
+ * 8.Get client Secret from backend via payment intent Post API
+ * 9.
  * 
  *  */ 
